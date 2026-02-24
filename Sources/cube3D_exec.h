c099e64f16f4d3db068ca63c3234b2c8308082bf
Maitathe-b <maiathenoob@gmail.com>
2026-02-24 17:38:11 +0000

    baby steps

--- Sources/cube3D_exec.h
+++ Sources/cube3D_exec.h
@@ -2,7 +2,7 @@
 # define CUBE3D_EXEC_H
 
 # include "../lib/libft/libft.h"
-# include "../lib/minilibx-linux/mlx.h"
+// # include "../lib/minilibx-linux/mlx.h"
 # include <X11/X.h>
 # include <X11/keysym.h>
 
@@ -14,5 +14,15 @@ enum e_textures
 	WEST
 };
 
-typdef struct 
+typedef struct s_vector
+{
+	double	camera_rotation;
+	double	vector_x;
+	double	vector_y;
+	int		map_x;
+	int		map_y;
+	
+}	t_vector;
+
+
 #endif
