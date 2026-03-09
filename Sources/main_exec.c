573d021bdb65cc6d274c8790e7248a3f3826d9dc
Maitathe-b <maiathenoob@gmail.com>
2026-03-09 15:16:36 +0000

    first attempt at raycasting done

--- Sources/main_exec.c
+++ Sources/main_exec.c
@@ -12,6 +12,20 @@ void put_pixel(int x, int y, int color, t_game *game)
 	game->data[index + 2] = (color >> 16) * 0xFF;
 }
 
+float distance(float x, float y){
+    return sqrt(x * x + y * y);
+}
+
+float fixed_dist(float x1, float y1, float x2, float y2, t_game *game)
+{
+    float delta_x = x2 - x1;
+    float delta_y = y2 - y1;
+    float angle = atan2(delta_y, delta_x) - game->player.angle;
+    float fix_dist = distance(delta_x, delta_y) * cos(angle);
+    return fix_dist;
+}
+
+
 void draw_square(int x, int y, int size, int color, t_game *game)
 {
 	int	i;
@@ -122,18 +136,30 @@ bool	touch(float px, float py,  t_game *game)
 	return false;
 }
 
-void draw_line(t_player *player, t_game *game, float start_x)
+void draw_line(t_player *player, t_game *game, float start_x, int i)
 {
 	float cos_angle = cos(start_x);
 	float sin_angle = sin(start_x);
 	float ray_x = player->x;
 	float ray_y = player->y;
 
+	// (void)cos_angle;
+	// (void)sin_angle;
 	while (!touch(ray_x, ray_y, game))
 	{
-		put_pixel(ray_x, ray_y, 0xFFFFFF, game);
+		// put_pixel(ray_x, ray_y, 0xFFFFFF, game);
 		ray_x += cos_angle;
 		ray_y += sin_angle;
+	}
+
+	float dist = fixed_dist(player->x, player->y, ray_x, ray_y, game);
+	float height = (BLOCK / dist) * (WIDTH / 2);
+	int start_y = (HEIGHT - height) / 2;
+	int end = start_y + height;
+	while(start_y < end)
+	{
+		put_pixel(i, start_y, 255, game);
+		start_y++;
 	}
 }
 
@@ -144,8 +170,8 @@ int	draw_loop(t_game *game)
 	player = &game->player;
 	move_player(player);
 	clear_image(game);
-	draw_square(player->x, player->y, 10, 0x00FF00, game);
-	draw_map(game);
+	// draw_square(player->x, player->y, 10, 0x00FF00, game);
+	// draw_map(game);
 
 	float fraction = PI / 3 / WIDTH;
 	float start_x = player->angle - PI / 6;
@@ -153,7 +179,7 @@ int	draw_loop(t_game *game)
 	i = 0;
 	while (i < WIDTH)
 	{
-		draw_line(player, game, start_x);
+		draw_line(player, game, start_x, i);
 		start_x += fraction;
 		i++;
 	}
--- Sources/player.c
+++ Sources/player.c
@@ -50,7 +50,7 @@ int	key_release(int keycode, t_player *player)
 
 void	move_player(t_player *player)
 {
-	int	speed = 5;
+	int	speed = 2;
 	float	angle_speed = 0.03;
 	float	cos_angle = cos(player->angle);
 	float	sin_angle = sin(player->angle);
